fb53f7ab9e9529e8a20d4466110c7112a0159d92
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2023-10-22 01:04:07 +0800

    Add files via upload

--- online-ordering-system-db.sql
+++ online-ordering-system-db.sql
@@ -16,20 +16,6 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`online_ordering_system_db` /*!40100 DEF
 
 USE `online_ordering_system_db`;
 
-/*Table structure for table `admins` */
-
-DROP TABLE IF EXISTS `admins`;
-
-CREATE TABLE `admins` (
-  `adminID` int(11) NOT NULL AUTO_INCREMENT,
-  `adminFirstname` varchar(100) NOT NULL,
-  `adminMiddlename` varchar(100) DEFAULT NULL,
-  `adminLastname` varchar(100) NOT NULL,
-  PRIMARY KEY (`adminID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `admins` */
-
 /*Table structure for table `employee_type` */
 
 DROP TABLE IF EXISTS `employee_type`;
@@ -98,16 +84,24 @@ DROP TABLE IF EXISTS `order_details`;
 CREATE TABLE `order_details` (
   `orderDetailID` int(11) NOT NULL AUTO_INCREMENT,
   `quantity` int(11) NOT NULL,
+  `orderDescription` varchar(255) NOT NULL,
   `orderID` int(11) NOT NULL,
   `productID` int(11) NOT NULL,
+  `productSizeID` int(11) NOT NULL,
   PRIMARY KEY (`orderDetailID`),
   KEY `transactionID` (`orderID`),
   KEY `productID` (`productID`),
+  KEY `productSizeID` (`productSizeID`),
   CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `order_details_ibfk_3` FOREIGN KEY (`productSizeID`) REFERENCES `product_sizes` (`productSizeID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `order_details` */
+
+insert  into `order_details`(`orderDetailID`,`quantity`,`orderDescription`,`orderID`,`productID`,`productSizeID`) values 
+(1,2,'Cheesy Bacon Fries',1,30,4),
+(2,3,'Blueberry Oreo',3,66,2);
 
 /*Table structure for table `orders` */
 
@@ -121,9 +115,17 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`orderID`),
   KEY `userID` (`userID`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `orders` */
+
+insert  into `orders`(`orderID`,`orderDate`,`status`,`userID`) values 
+(1,'2023-10-21','Order Processed',1),
+(2,'2023-10-22','Order Processed',2),
+(3,'2023-10-23','Order Processed',4),
+(4,'2023-10-23','Order Processed',5),
+(5,'2023-11-13','Order Processed',8),
+(6,'2023-11-15','Order Processed',7);
 
 /*Table structure for table `payment_methods` */
 
@@ -163,9 +165,26 @@ CREATE TABLE `product_categories` (
   `categoryID` int(11) NOT NULL AUTO_INCREMENT,
   `categoryName` varchar(30) NOT NULL,
   PRIMARY KEY (`categoryID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `product_categories` */
+
+insert  into `product_categories`(`categoryID`,`categoryName`) values 
+(1,'Burger & Fries'),
+(2,'Sandwiches & Fries'),
+(3,'Sizzling Meals'),
+(4,'Ala Carte Wings'),
+(5,'Pica Pica'),
+(6,'Fries'),
+(8,'Rice'),
+(9,'Classic Milktea'),
+(10,'Frappe'),
+(11,'Premium Milktea'),
+(12,'Cheesecake Series'),
+(13,'Fruit Tea'),
+(14,'Iced Coffee'),
+(15,'Hot Coffee'),
+(16,'Sinkers');
 
 /*Table structure for table `product_flavors` */
 
@@ -175,9 +194,25 @@ CREATE TABLE `product_flavors` (
   `flavorID` int(11) NOT NULL AUTO_INCREMENT,
   `flavorName` varchar(100) NOT NULL,
   PRIMARY KEY (`flavorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `product_flavors` */
+
+insert  into `product_flavors`(`flavorID`,`flavorName`) values 
+(1,'Sweet & Spicy'),
+(2,'Buffalo'),
+(3,'Super Sili'),
+(4,'Garlic Parmesan'),
+(5,'Honey Garlic'),
+(6,'Garlic Mayo'),
+(7,'Classic Gravy'),
+(8,'Teriyaki'),
+(9,'Barbeque'),
+(10,'Soy Garlic'),
+(11,'Mango Habanero'),
+(12,'Lemon Glazed'),
+(13,'Spiced Maple'),
+(14,'Salted Egg');
 
 /*Table structure for table `product_sizes` */
 
@@ -186,12 +221,34 @@ DROP TABLE IF EXISTS `product_sizes`;
 CREATE TABLE `product_sizes` (
   `productSizeID` int(11) NOT NULL AUTO_INCREMENT,
   `productSizeName` varchar(20) NOT NULL,
-  `productSize` int(11) NOT NULL,
+  `productSize` varchar(20) NOT NULL,
   `productSizePrice` double NOT NULL,
   PRIMARY KEY (`productSizeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `product_sizes` */
+
+insert  into `product_sizes`(`productSizeID`,`productSizeName`,`productSize`,`productSizePrice`) values 
+(1,'Lowdose','16oz',79),
+(2,'Highdose','22oz',99),
+(3,'Overdose','1Liter',119),
+(4,'NA','NA',0);
+
+/*Table structure for table `product_type` */
+
+DROP TABLE IF EXISTS `product_type`;
+
+CREATE TABLE `product_type` (
+  `productTypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `productType` varchar(100) NOT NULL,
+  PRIMARY KEY (`productTypeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `product_type` */
+
+insert  into `product_type`(`productTypeID`,`productType`) values 
+(1,'Food'),
+(2,'Beverage');
 
 /*Table structure for table `products` */
 
@@ -202,12 +259,103 @@ CREATE TABLE `products` (
   `productName` varchar(30) NOT NULL,
   `productPrice` double NOT NULL,
   `categoryID` int(11) NOT NULL,
+  `productTypeID` int(11) NOT NULL,
   PRIMARY KEY (`productID`),
   KEY `categoryID` (`categoryID`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `product_categories` (`categoryID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `productTypeID` (`productTypeID`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `product_categories` (`categoryID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`productTypeID`) REFERENCES `product_type` (`productTypeID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `products` */
+
+insert  into `products`(`productID`,`productName`,`productPrice`,`categoryID`,`productTypeID`) values 
+(1,'Classic Burger',110,1,1),
+(2,'Cheesy Bacon Beef Burger',130,1,1),
+(3,'Double Patty',180,1,1),
+(4,'Big Tower',200,1,1),
+(5,'Clubhouse',100,2,1),
+(6,'Ham & Cheese Toast',80,2,1),
+(7,'Sizzling Hotdog',89,3,1),
+(8,'Sizzling Tapa',99,3,1),
+(9,'Sizzling Tocino',99,3,1),
+(10,'Sizzling Burger Steak(2pcs)',99,3,1),
+(11,'Sizzling Pork-Chop',129,3,1),
+(12,'Sizzling Liempo',139,3,1),
+(13,'Sizzling Wings(3pcs)',149,3,1),
+(14,'Sizzling Bangus',139,3,1),
+(15,'3pcs Wings w/rice',115,4,1),
+(16,'3pcs Wings w/fries',115,4,1),
+(17,'3pcs Wings w/burger',199,4,1),
+(18,'Kikiam',50,5,1),
+(19,'Squidballs',50,5,1),
+(20,'Cheesy Nachos',80,5,1),
+(21,'CheeseSticks',70,5,1),
+(22,'Mojos',123,5,1),
+(23,'Hashbrown',50,5,1),
+(24,'Hotdog & Fries',120,5,1),
+(25,'Meaty Nachos',120,5,1),
+(26,'Plain Fries (Salted)',80,6,1),
+(27,'Cheese Fries',80,6,1),
+(28,'Sour & Cream Fries',80,6,1),
+(29,'BBQ Fries',80,6,1),
+(30,'Chessy Bacon Fries',120,6,1),
+(38,'Plain Rice',20,8,1),
+(39,'Java Rice',20,8,1),
+(40,'Cookies And Cream',0,9,2),
+(41,'Chocolate',0,9,2),
+(42,'Dark Choco',0,9,2),
+(43,'Strawberry',0,9,2),
+(44,'Red Velvet',0,9,2),
+(45,'Okinawa',0,9,2),
+(46,'Hokkaido',0,9,2),
+(47,'Matcha',0,9,2),
+(48,'Wintermelon',0,9,2),
+(49,'Salted Caramel',0,9,2),
+(50,'Double Dutch',0,9,2),
+(51,'Cookies And Cream',0,10,2),
+(52,'Choco',0,10,2),
+(53,'Dark Choco',0,10,2),
+(54,'Strawberry',0,10,2),
+(55,'Red Velvet',0,10,2),
+(56,'Matcha',0,10,2),
+(57,'Salted Caramel',0,10,2),
+(58,'Double Dutch',0,10,2),
+(59,'Blue Berry',0,10,2),
+(60,'Java Chip',0,10,2),
+(61,'Cappuccino',0,10,2),
+(62,'Nutella Orea',0,11,2),
+(63,'Apollo Nutella',0,11,2),
+(64,'Milo Kingkong',0,11,2),
+(65,'Tiger Sugar',0,11,2),
+(66,'Blueberry Oreo',0,12,2),
+(67,'Oreo Cheesecake',0,12,2),
+(68,'Choco Gaze',0,12,2),
+(69,'Strawberry Oreo',0,12,2),
+(70,'Red Velvet',0,12,2),
+(71,'Matcha Oreo',0,12,2),
+(72,'Okinawa',0,12,2),
+(73,'Taro Oreo',0,12,2),
+(74,'Dark Choco Oreo',0,12,2),
+(75,'Lychee',0,13,2),
+(76,'Blueberry',0,13,2),
+(77,'Green Apple',0,13,2),
+(78,'Strawberry',0,13,2),
+(79,'Watermelon',0,13,2),
+(80,'Barako',80,14,2),
+(81,'Caramel Macchiato',110,14,2),
+(82,'Sakura Latte',110,14,2),
+(83,'Matcha Latte',0,14,2),
+(84,'BARAKO',45,15,2),
+(85,'SB Capuccion',130,15,2),
+(86,'SB House Blend',100,15,2),
+(87,'SB Espresso Roast',100,15,2),
+(88,'Caffe Latte',100,15,2),
+(89,'Pearl',20,16,2),
+(90,'Nata',20,16,2),
+(91,'Crystall Jelly',20,16,2),
+(92,'Oreo',20,16,2),
+(93,'Cheesecake',25,16,2);
 
 /*Table structure for table `suppliers` */
 
@@ -269,19 +417,93 @@ CREATE TABLE `users` (
   `userID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL,
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `users` */
 
-insert  into `users`(`userID`,`username`,`password`) values 
-(1,'admin','$2y$10$uFWl43sg9gJNG9ZdAkUtQeJc0kxK/QHcSMxbSsIZE6j1Lc1B/6PEu'),
-(2,'allen','$2y$10$.paKB11AgpXWbzWJhyu6K.aio4ldIKTYY6xl5gKIBiLSjdA/W4mGS'),
-(4,'admin2','$2y$10$0hYbg4fSikEzKm.cdbqUcekwrJ17KGVhXqcCSJaH5V3M6G05k9lbu'),
-(5,'admin2','$2y$10$8MYRb1QoLhEkbU90VJ.8CeOjiZOFDW7OZLMIyr/L5m8vDiXZXD1yS'),
-(6,'admin3','$2y$10$lZtei7ReFxsDR65KJeQhLO//kBNxU.arysgCN.DQbEhuSacFrVxpS'),
-(7,'admin3','$2y$10$BWQmjP0LK4LXDCkvhIUZmeN6F0dvw0yoiEingYU8uj/3.QCdLjBRO'),
-(8,'jherico','$2y$10$Y5LxC3pkUEiytWuz7SCD8OnkT0okmnKEP89HLoMBBbeFE9Tmx794C');
+insert  into `users`(`userID`,`username`,`password`,`role`) values 
+(1,'admin','$2y$10$uFWl43sg9gJNG9ZdAkUtQeJc0kxK/QHcSMxbSsIZE6j1Lc1B/6PEu','admin'),
+(2,'allen','$2y$10$.paKB11AgpXWbzWJhyu6K.aio4ldIKTYY6xl5gKIBiLSjdA/W4mGS','user'),
+(4,'admin2','$2y$10$0hYbg4fSikEzKm.cdbqUcekwrJ17KGVhXqcCSJaH5V3M6G05k9lbu','admin'),
+(5,'admin2','$2y$10$8MYRb1QoLhEkbU90VJ.8CeOjiZOFDW7OZLMIyr/L5m8vDiXZXD1yS','admin'),
+(6,'admin3','$2y$10$lZtei7ReFxsDR65KJeQhLO//kBNxU.arysgCN.DQbEhuSacFrVxpS','admin'),
+(7,'admin3','$2y$10$BWQmjP0LK4LXDCkvhIUZmeN6F0dvw0yoiEingYU8uj/3.QCdLjBRO','admin'),
+(8,'jherico','$2y$10$Y5LxC3pkUEiytWuz7SCD8OnkT0okmnKEP89HLoMBBbeFE9Tmx794C','admin');
+
+/*Table structure for table `view_orders` */
+
+DROP TABLE IF EXISTS `view_orders`;
+
+/*!50001 DROP VIEW IF EXISTS `view_orders` */;
+/*!50001 DROP TABLE IF EXISTS `view_orders` */;
+
+/*!50001 CREATE TABLE  `view_orders`(
+ `orderDate` varchar(73) ,
+ `status` varchar(100) 
+)*/;
+
+/*Table structure for table `view_product_count` */
+
+DROP TABLE IF EXISTS `view_product_count`;
+
+/*!50001 DROP VIEW IF EXISTS `view_product_count` */;
+/*!50001 DROP TABLE IF EXISTS `view_product_count` */;
+
+/*!50001 CREATE TABLE  `view_product_count`(
+ `productCount` bigint(21) 
+)*/;
+
+/*Table structure for table `view_total_sales_month` */
+
+DROP TABLE IF EXISTS `view_total_sales_month`;
+
+/*!50001 DROP VIEW IF EXISTS `view_total_sales_month` */;
+/*!50001 DROP TABLE IF EXISTS `view_total_sales_month` */;
+
+/*!50001 CREATE TABLE  `view_total_sales_month`(
+ `totalSales` double 
+)*/;
+
+/*Table structure for table `view_user_count` */
+
+DROP TABLE IF EXISTS `view_user_count`;
+
+/*!50001 DROP VIEW IF EXISTS `view_user_count` */;
+/*!50001 DROP TABLE IF EXISTS `view_user_count` */;
+
+/*!50001 CREATE TABLE  `view_user_count`(
+ `userCount` bigint(21) 
+)*/;
+
+/*View structure for view view_orders */
+
+/*!50001 DROP TABLE IF EXISTS `view_orders` */;
+/*!50001 DROP VIEW IF EXISTS `view_orders` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_orders` AS select date_format(`orders`.`orderDate`,'%M %d, %Y') AS `orderDate`,`orders`.`status` AS `status` from `orders` */;
+
+/*View structure for view view_product_count */
+
+/*!50001 DROP TABLE IF EXISTS `view_product_count` */;
+/*!50001 DROP VIEW IF EXISTS `view_product_count` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_product_count` AS select count(0) AS `productCount` from `products` */;
+
+/*View structure for view view_total_sales_month */
+
+/*!50001 DROP TABLE IF EXISTS `view_total_sales_month` */;
+/*!50001 DROP VIEW IF EXISTS `view_total_sales_month` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_total_sales_month` AS select sum(if(`ps`.`productSize` = 'NA',`od`.`quantity` * `pd`.`productPrice`,`od`.`quantity` * `ps`.`productSizePrice`)) AS `totalSales` from ((((`order_details` `od` left join `orders` `or` on(`or`.`orderID` = `od`.`orderID`)) left join `product_sizes` `ps` on(`od`.`productSizeID` = `ps`.`productSizeID`)) left join `products` `pd` on(`pd`.`productID` = `od`.`productID`)) left join `product_type` `pt` on(`pd`.`productTypeID` = `pt`.`productTypeID`)) group by `or`.`orderDate` = month(curdate()) */;
+
+/*View structure for view view_user_count */
+
+/*!50001 DROP TABLE IF EXISTS `view_user_count` */;
+/*!50001 DROP VIEW IF EXISTS `view_user_count` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_user_count` AS select count(0) AS `userCount` from `users` */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
